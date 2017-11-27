d6821188b8b9d17aae8bdb617e0379914f450100
CHAITANYA <CHAITANYA@NVRCHAITANYA>
2017-11-27 23:43:31 +0530

    FIRST COMMIT

---
+++ src/main/java/hello/ChaiSprinWsApplication.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class ChaiSprinWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaiSprinWsApplication.class, args);
+	}
+}
---
+++ src/main/java/hello/CountryEndpoint.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import io.spring.guides.gs_producing_web_service.GetCountryRequest;
+import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+
+/**
+ * @author CHAITANYA
+ *
+ */
+@Endpoint
+public class CountryEndpoint {
+	
+	private static final String NAMESPACE_URI ="http://spring.io/guides/gs-producing-web-service";
+	
+	private CountryRepository countryRepository;
+	
+	
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		
+		this.countryRepository=countryRepository;
+	}
+	
+	
+	@PayloadRoot(namespace=NAMESPACE_URI,localPart="getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse  getCountryInfo(@RequestPayload GetCountryRequest getCountryRequest ) {
+		
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(getCountryRequest.getName()));
+		return response;
+	}
+
+}
